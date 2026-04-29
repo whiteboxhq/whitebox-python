a767fc7311878956805a304dc05fba022b91184e
Javier Roque <jarb95@gmail.com>
2026-04-29 16:31:42 -0600

    v0.1.1: add models param, list_models(), Batch.is_complete

--- whitebox/__init__.py
+++ whitebox/__init__.py
@@ -2,7 +2,7 @@ from .client import Whitebox
 from .models import Decision, Batch, Review, Run
 from .exceptions import WhiteboxError, AuthenticationError, RateLimitError, InsufficientCreditsError
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [
     "Whitebox",
     "Decision",
